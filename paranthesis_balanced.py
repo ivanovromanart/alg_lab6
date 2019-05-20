304bb6792174e73aa22070df8113488524929684
ivanovromanart <44047585+ivanovromanart@users.noreply.github.com>
2019-05-20 21:23:55 +0700

    Add files via upload

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 def is_paranthesis_balanced(s):
-    pass
-
+    l={')':'(',']':'[','}':'{','>':'<' }
+    a = []
+    if len(s)==0:
+        return True
+    for i in s:
+        if i in l and len(a)>0 and l[i]==a[-1]:
+            a.pop()
+        else:
+            a.append(i)
+    if len(a)>0:
+        return False
+    else:
+        return True
